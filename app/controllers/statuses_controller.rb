d2e64e2ffeee061243d8da07092040813df42d20
Jeremy G. Bray <j_bray14@yahoo.com>
2013-11-15 14:57:08 -0600

    Updated fronted for the new authentication system.
    Added simple_form
    Updated several forms to bootstrap/simple_forn
    Added login/logout/register/profile links to our nav
    Added a collection select to our status form

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -27,7 +27,7 @@ class StatusesController < ApplicationController
     @status = Status.new
 
     respond_to do |format|
-      format.html # new.html.erb
+      format.html  #new.html.erb
       format.json { render json: @status }
     end
   end
